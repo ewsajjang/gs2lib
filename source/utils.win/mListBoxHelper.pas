e082547ba706cd47f15d6a4dbaeb23779a77c170
gomsun2 <gomsun2@gmail.com>
2014-10-15 11:18:20 +0900

    Added TListBoxHelper

---
+++ source/utils.win/mListBoxHelper.pas
@@ -0,0 +1,37 @@
+unit mListBoxHelper;
+
+interface
+
+uses
+  System.Classes, System.SysUtils,
+  Vcl.StdCtrls
+  ;
+
+type
+  TListBoxHelper = class helper for TListBox
+    function ItemSelected: Boolean;
+    function ItemText: String;
+  end;
+
+implementation
+
+uses
+  mConsts;
+
+{ TListBoxHelper }
+
+function TListBoxHelper.ItemSelected: Boolean;
+begin
+  Result := ItemIndex > VAL_NOT_ASSIGNED;
+end;
+
+function TListBoxHelper.ItemText: String;
+begin
+  if ItemSelected then
+    Result := Items[ItemIndex]
+  else
+    Result := EmptyStr;
+end;
+
+end.
+
