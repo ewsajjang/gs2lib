f049f508e02b23952b127cadb0291427258811d9
gomsun2 <gomsun2@gmail.com>
2014-10-15 11:30:16 +0900

    Improved code readable

--- source/utils/mConsts.pas
+++ source/utils/mConsts.pas
@@ -3,7 +3,9 @@ unit mConsts;
 interface
 
 const
-  BoolStr: array[False..True] of string = ('False', 'True');
+	FALSE_STR = 'False';
+  TRUE_STR = 'True';
+  BoolStr: array[False..True] of string = (FALSE_STR, TRUE_STR);
   VAL_NOT_ASSIGNED = -1;
 
 implementation
