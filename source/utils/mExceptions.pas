33a7289774fdc2e0ea6f5b54a933977e1b73e21f
gomsun2 <gomsun2@gmail.com>
2015-12-22 17:18:37 +0900

    Added mExceptions.pas

---
+++ source/utils/mExceptions.pas
@@ -0,0 +1,31 @@
+unit mExceptions;
+
+interface
+
+uses
+  System.Classes, System.SysUtils
+  ;
+
+type
+  EAbstractException = class(Exception)
+    constructor Create(const Msg: String); overload; virtual;
+    constructor CreateFmt(const Msg: String; Args: array of const); overload; virtual;
+  end;
+  ELogicalError = class(EAbstractException);
+
+
+implementation
+
+{ EAbstractException }
+
+constructor EAbstractException.Create(const Msg: String);
+begin
+  inherited Create(Msg);
+end;
+
+constructor EAbstractException.CreateFmt(const Msg: String; Args: array of const);
+begin
+  inherited CreateFmt(Msg, Args);
+end;
+
+end.
