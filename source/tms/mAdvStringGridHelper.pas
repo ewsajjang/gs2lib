b7f606884a1400b77a2b3dceef5ee7ace03adbbe
gomsun2 <gomsun2@gmail.com>
2016-03-08 17:21:51 +0900

    Added TAdvStringGirdHelper

---
+++ source/tms/mAdvStringGridHelper.pas
@@ -0,0 +1,20 @@
+unit mAdvStringGridHelper;
+
+interface
+
+uses
+  AdvGrid,
+
+  mStringGridHelper,
+
+  System.SysUtils, System.Classes
+  ;
+
+type
+  TAdvStringGridHelper = class helper(TStringGridHelper) for TAdvStringGrid
+  end;
+
+
+implementation
+
+end.
