ff5b313511df1b88b4fbb81b06796233220e7d8e
gomsun2 <gomsun2@gmail.com>
2016-08-11 14:57:19 +0900

    Edited TBytesStreamHelper. Renamed PostionOf to PosOfStart. Added PosOfEnd

--- source/utils/mStreamHelper.pas
+++ source/utils/mStreamHelper.pas
@@ -9,21 +9,54 @@ uses
 type
   TBytesStreamHelper = class helper for TBytesStream
   public
-    function PositionOf(const ATarget: TBytes; const AStartPos: Integer = 0): Integer; overload;
-    function PositionOf(const ATarget: TBytes; const AStartPos: Integer; var AFindIdx: Integer): Boolean; overload;
+    function PosOfStart(const ATarget: TBytes; const AStartPos: Integer = 0): Integer; overload;
+    function PosOfStart(const ATarget: TBytes; const AStartPos: Integer; var AFindIdx: Integer): Boolean; overload;
+    function PosOfEnd(const ATarget: TBytes; var AFindIdx: Integer): Boolean; overload;
+    function PosOfEnd(const ATarget: TBytes): Integer; overload;
   end;
 
 implementation
 
+uses
+  System.Math
+  ;
+
 { TBytesStreamHelper }
 
-function TBytesStreamHelper.PositionOf(const ATarget: TBytes; const AStartPos: Integer): Integer;
+function TBytesStreamHelper.PosOfStart(const ATarget: TBytes; const AStartPos: Integer): Integer;
 begin
-  if not PositionOf(ATarget, AStartPos, Result) then
+  if not PosOfStart(ATarget, AStartPos, Result) then
     Result := -1;
 end;
 
-function TBytesStreamHelper.PositionOf(const ATarget: TBytes;
+function TBytesStreamHelper.PosOfEnd(const ATarget: TBytes;
+  var AFindIdx: Integer): Boolean;
+var
+  LEnd, LTargetLen, i: Integer;
+begin
+  Result := False;
+  LTargetLen := Length(ATarget);
+  LEnd := Size;
+  AFindIdx := LEnd -LTargetLen;
+  while InRange(AFindIdx, 0, LEnd -LTargetLen) do
+  begin
+    i := 0;
+    while (i < LTargetLen) and (Bytes[AFindIdx + i] = ATarget[i]) do
+      Inc(i);
+    Result := i = LTargetLen;
+    if Result then
+      Break;
+    Dec(AFindIdx);
+  end;
+end;
+
+function TBytesStreamHelper.PosOfEnd(const ATarget: TBytes): Integer;
+begin
+  if not PosOfEnd(ATarget, Result) then
+    Result := -1;
+end;
+
+function TBytesStreamHelper.PosOfStart(const ATarget: TBytes;
   const AStartPos: Integer; var AFindIdx: Integer): Boolean;
 var
   i: Integer;
