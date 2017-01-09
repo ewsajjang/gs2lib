1cf0a09ed8c926c01e0fc4cc78e86ff76e4711e8
jhjang <gomsun2@gmail.com>
2017-01-09 12:21:56 +0900

    개선, fmxForm의  placeOn

--- source/mvw.fmx/mvw.vform.pas
+++ source/mvw.fmx/mvw.vform.pas
@@ -13,41 +13,17 @@ type
   TFmxObjectEnumProc<T: class> = reference to procedure(ACtrl: T; ACtrlName: String);
   TFmxObjectEnumProc = reference to procedure(ACtrl: TFmxObject; ACtrlName: String);
   TvForm = class(TForm)
-  private class var
-    FDic: TDictionary<String, TvForm>;
   private
-    function GetVCnt: Integer;
-    function GetVName(AClassName: String): TvForm;
   protected
     FPlaceOnProc: TProc;
   public
-    constructor Create(AOwner: TComponent); override;
-    destructor Destroy; override;
-
-    procedure PlaceOn(const AForm: TvForm; AContainer: TFmxObject);
+    class function PlaceOn<T: TForm>(AContainer: TFmxObject): T; overload;
+    procedure PlaceOn(const AForm: TvForm; AContainer: TFmxObject); overload;
     procedure FmxObjectEnum<T: class>(AProc: TFmxObjectEnumProc<T>; const AContainer: TFmxObject = nil); overload;
     procedure FmxObjectEnum(AProc: TFmxObjectEnumProc; const AContainer: TFmxObject = nil); overload;
-    function vExists(const AClassName: String): Boolean;
-
-
-    property vCnt: Integer read GetVCnt;
-    property vNames[AClassName: String]: TvForm read GetVName;
-  end;
-
-  Tv<Tvm: class> = class(TvForm)
-  private
-    function Getvm: Tvm;
-  protected
-    function Query<Tv: class>: TvForm;
-  public
-    property vm: Tvm read Getvm;
   end;
 
 implementation
-
-uses
-  mvw.Services
-  ;
 
 { TvForm }
 
@@ -78,35 +54,6 @@ begin
     AContainer);
 end;
 
-constructor TvForm.Create(AOwner: TComponent);
-begin
-  inherited;
-
-  if not Assigned(FDic) then
-    FDic := TDictionary<String, TvForm>.Create;
-  FDic.Add(ClassName, Self);
-end;
-
-destructor TvForm.Destroy;
-begin
-  FDic.Remove(ClassName);
-  if FDic.Count = 0 then
-    FreeAndNil(FDic);
-
-  inherited;
-end;
-
-function TvForm.GetVCnt: Integer;
-begin
-  Result := FDic.Count;
-end;
-
-function TvForm.GetVName(AClassName: String): TvForm;
-begin
-  if not FDic.TryGetValue(AClassName, Result) then
-    Result := nil;
-end;
-
 procedure TvForm.PlaceOn(const AForm: TvForm; AContainer: TFmxObject);
 begin
   if AForm.ChildrenCount > 0 then
@@ -119,21 +66,12 @@ begin
   end;
 end;
 
-function TvForm.vExists(const AClassName: String): Boolean;
+class function TvForm.PlaceOn<T>(AContainer: TFmxObject): T;
 begin
-  Result := FDic.ContainsKey(AClassName)
-end;
-
-{ Tv<Tvm> }
-
-function Tv<Tvm>.Getvm: Tvm;
-begin
-  Result := vmList.Get<Tvm>;
-end;
-
-function Tv<Tvm>.Query<Tv>: TvForm;
-begin
-  Result := FDic.Items[Tv.ClassName];
+  Application.CreateForm(T, Result);
+  Result.BorderStyle := TFmxFormBorderStyle.None;
+  while Result.ChildrenCount>0 do
+    Result.Children[0].Parent := AContainer;
 end;
 
 end.
