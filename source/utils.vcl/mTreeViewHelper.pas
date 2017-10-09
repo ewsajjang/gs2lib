9b7f2b0d790c362ebc97f67608ee525646bb5209
gomsun2 <gomsun2@gmail.com>
2017-10-09 17:14:37 +0900

    추가, TTreeView에서 Collapse 를 CrossAroww를 선택할 수 있도록 ExplorerTheme 추가

--- source/utils.vcl/mTreeViewHelper.pas
+++ source/utils.vcl/mTreeViewHelper.pas
@@ -7,12 +7,22 @@ uses
   ;
 
 type
+  TTreeViewHelper = class helper for TTreeView
+  private
+    procedure SetExplorerTheme(const Value: Boolean);
+  public
+    property ExplorerTheme: Boolean write SetExplorerTheme;
+  end;
   TTreeNodeHelper = class helper for TTreeNodes
     function AddChildFmt(Parent: TTreeNode; const S: string; const Args: array of const): TTreeNode;
     function AddChildObjectFmt(Parent: TTreeNode; const S: string; const Args: array of const; Ptr: Pointer): TTreeNode;
   end;
 
 implementation
+
+uses
+  Vcl.Themes, Winapi.UxTheme
+  ;
 
 { TTreeNodeHelper }
 
@@ -26,6 +36,18 @@ function TTreeNodeHelper.AddChildObjectFmt(Parent: TTreeNode; const S: string;
   const Args: array of const; Ptr: Pointer): TTreeNode;
 begin
   Result := AddChildObject(Parent, Format(S, Args), Ptr);
+end;
+
+{ TTreeViewHelper }
+
+procedure TTreeViewHelper.SetExplorerTheme(const Value: Boolean);
+begin
+  // fork from - https://stackoverflow.com/a/8408192/1174572
+  if ThemeServices.ThemesEnabled and CheckWin32Version(6, 0) then
+    if Value then
+      SetWindowTheme(Handle, 'Explorer', nil)
+    else
+      SetWindowTheme(Handle, nil, nil);
 end;
 
 end.
